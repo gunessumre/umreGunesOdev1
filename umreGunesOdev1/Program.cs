52715fc1db744e5377f8ab4abe8ecf34286c3a42
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-09-25 14:14:29 +0300

    umreGunesOdev1 eklendi

---
+++ umreGunesOdev1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UmreGunesOdev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Umre Gunes");
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!");
+        }
+    }
+}
